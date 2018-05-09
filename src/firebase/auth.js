f5fb2ba473b2e2672ea0dff44144f1c0266da83d
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-05-09 00:01:57 -0400

    Firebase setup

---
+++ src/firebase/auth.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyA62nY1-Y5tW4MLGpae8q-grcaa-Ai18h4",
+    authDomain: "aprendendo-firebase-c5341.firebaseapp.com",
+    databaseURL: "https://aprendendo-firebase-c5341.firebaseio.com",
+    projectId: "aprendendo-firebase-c5341",
+    storageBucket: "aprendendo-firebase-c5341.appspot.com",
+    messagingSenderId: "387535528075"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+    auth,
+};
