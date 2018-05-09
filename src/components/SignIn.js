df0d4b122ae5caa5bb6da8979872bb707eeb8eda
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-05-09 11:21:12 -0400

    Password Reset and Password Change with Firebase

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-
+import { PasswordForgetLink } from './PasswordForget';
 import { SignUpLink } from './SignUp';
 import { auth } from '../firebase';
 import * as routes from '../constants/routes';
@@ -9,6 +9,7 @@ const SignInPage = ({ history }) =>
     <div>
         <h1>SignIn</h1>
         <SignInForm history={history} />
+        <PasswordForgetLink />
         <SignUpLink />
     </div>
 
