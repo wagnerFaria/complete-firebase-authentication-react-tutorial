98e4f9959a18faa38123c3802dfe0d68ae2cb4fe
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-05-09 14:05:51 -0400

    Corrigido erro ao fazer logout

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,13 +1,29 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import { auth } from '../firebase';
+import { withRouter } from 'react-router-dom';
+import * as routes from '../constants/routes';
 
-const SignOutButton = () =>
-    <button
-        type="button"
-        onClick={auth.doSignOut}
-    >
-        Sign Out
-  </button>
+class SignOut extends Component {
 
-export default SignOutButton;
+    onClicked = () => {
+        auth.doSignOut().then(()=>{
+            this.props.history.push(routes.LANDING);
+        }).catch((error) => {
+            console.log(error);
+        });
+        
+    };
+    render() {
+        return (
+            <div>
+                <button
+                        type="button"
+                        onClick={this.onClicked}>
+                        Sign Out
+                </button>
+            </div>
+        );
+    }
+}
+
+export default withRouter(SignOut);
