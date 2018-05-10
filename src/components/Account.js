a19f6c490bc881a417eab50758a9ca01d185febc
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-05-09 23:29:43 -0400

    Protected Routes in React with Authorization

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,16 +3,19 @@ import React from 'react';
 import AuthUserContext from './AuthUserContext';
 import { PasswordForgetForm } from './PasswordForget';
 import PasswordChangeForm from './PasswordChange';
+import withAuthorization from './withAuthorization';
 
 const AccountPage = () =>
     <AuthUserContext.Consumer>
         {authUser =>
             <div>
-                <h1>Account: {authUser.email}</h1>
+                <h1>Account: {authUser ? authUser.email : null}</h1>
                 <PasswordForgetForm />
                 <PasswordChangeForm />
             </div>
         }
     </AuthUserContext.Consumer>
 
-export default AccountPage;
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(AccountPage);
